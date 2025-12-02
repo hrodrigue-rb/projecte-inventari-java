49bff77914b33c8aee25a307cd123648132b685d
hrodrigue-rb <hrodrigue@inscamidemar.cat>
2025-12-02 13:47:07 +0100

    Merge all changes into main

---
+++ src/projecte_inventari_java/Categoria.java
@@ -0,0 +1,13 @@
+package projecte_inventari_java;
+
+/**
+ * Enum que defineix les categories de material dins de l'inventari.
+ */
+public enum Categoria {
+    LLIBRE_TEXT,
+    MATERIAL_ESCOLAR,
+    INFORMÀTICA,
+    LABORATORI,
+    EDUCACIO_FISICA,
+    MATERIAL_ARTISTIC
+}
