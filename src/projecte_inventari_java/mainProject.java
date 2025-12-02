8d602f1ac226532f7d8703d7b70cee45f6f54196
HectorzBarz <hrodrigue@inscamidemar.cat>
2025-12-02 13:52:28 +0100

    feat: add canviarDisponibilitatMaterial to comply the 5 functions condition

--- src/projecte_inventari_java/mainProject.java
+++ src/projecte_inventari_java/mainProject.java
@@ -27,6 +27,7 @@ public class mainProject {
             System.out.println("2. Veure disponibilitat");
             System.out.println("3. Afegir material");
             System.out.println("4. Prestar material");
+            System.out.println("5. Canviar disponibilitat manualment");
             System.out.println("99. Sortir");
             System.out.print("Opció: ");
             opcioMenu = scannerEntrada.nextInt();
@@ -71,6 +72,16 @@ public class mainProject {
                     boolean materialPrestatCorrectament = serveiInventari.prestarMaterial(idMaterial);
                     System.out.println(materialPrestatCorrectament ? "Material prestat amb èxit!" 
                                                                     : "No es pot prestar: no existeix o ja no està disponible.");
+                    break;
+
+                case 5:
+                    System.out.print("Introdueix l'ID del material: ");
+                    idMaterial = scannerEntrada.nextInt();
+                    System.out.print("Nova disponibilitat (true/false): ");
+                    boolean novaDisponibilitat = scannerEntrada.nextBoolean();
+                    boolean canviCorrecte = serveiInventari.canviarDisponibilitatMaterial(idMaterial, novaDisponibilitat);
+                    System.out.println(canviCorrecte ? "Disponibilitat actualitzada correctament." 
+                                                      : "No s'ha pogut actualitzar: material no existeix.");
                     break;
 
                 case 99:
